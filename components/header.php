ea860dd38bda993a356a9aaf127d0daa2f330577
Avi <67220821+avi-lime@users.noreply.github.com>
2023-01-29 20:39:15 +0530

    favicons

--- components/header.php
+++ components/header.php
@@ -8,6 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Memento | Apparels, Cool Designs & Everything In Between.</title>
+    <!-- Favicons -->
+    <link rel="icon" type="image/x-icon" href="img/favicons/mxm-favicon-32.png" sizes="any">
+    <link rel="shortcut icon" href="img/favicons/mxm-android.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/favicons/mxm-ios.png">
+
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
